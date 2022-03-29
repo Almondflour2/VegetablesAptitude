c2268cafa16c037f53e318b84934e21b3e5fce90
Isabella Almond <almondflour2@gmail.com>
2022-03-28 22:08:41 -0400

    build commit to get it to run on other computer

--- VegetablesAptitude/VegetablesAptitude/Program.cs
+++ VegetablesAptitude/VegetablesAptitude/Program.cs
@@ -5,82 +5,96 @@ namespace VegetablesAptitude
 {
     class Program
     {
-        private static List<Quiz> _takers = new List<Quiz>();
-        static void Main(string[] args)
+        private static List<Quiz> _takers = new List<Quiz>(); //creates list of takers - instances of Quiz 
+        static void Main(string[] args) //main method
         {
-            // Menu Loop
-            Console.WriteLine("hey sprout...!");
+            //startup text
+            string ascii = @"
 
+                      (`-')(`-')  _          (`-')  _(`-')    (`-')  _ <-.(`-')         (`-')  _ (`-').-> 
+                     _(OO )( OO).-/    .->   ( OO).-/( OO).-> (OO ).-/  __( OO)   <-.   ( OO).-/ ( OO)_   
+                ,--.(_/,-.(,------. ,---(`-'|,------./    '._ / ,---.  '-'---.\ ,--. ) (,------.(_)--\_)  
+                \   \ / (_/|  .---''  .-(OO )|  .---'|'--...__) \ /`.\ | .-. (/ |  (`-')|  .---'/    _ /  
+                 \   /   /(|  '--. |  | .-, (|  '--. `--.  .--'-'|_.' || '-' `.)|  |OO ||  '--. \_..`--.  
+                _ \     /_)|  .--' |  | '.(_/|  .--'    |  | (|  .-.  || /`'.  (|  '__ ||  .--' .-._)   \ 
+                \-'\   /   |  `---.|  '-'  | |  `---.   |  |  |  | |  || '--'  /|     |'|  `---.\       / 
+                    `-'    `------' `-----'  `------'   `--'  `--' `--'`------' `-----' `------' `-----'  
+                         (`-')  _  _  (`-')(`-')      _     (`-')                _(`-')   (`-')  _                
+                         (OO ).-/  \-.(OO )( OO).->  (_)    ( OO).->       .->  ( (OO ).->( OO).-/                
+                         / ,---.   _.'    \/    '._  ,-(`-')/    '._  ,--.(,--.  \    .'_(,------.                
+                         | \ /`.\ (_...--''|'--...__)| ( OO)|'--...__)|  | |(`-')'`'-..__)|  .---'                
+                         '-'|_.' ||  |_.' |`--.  .--'|  |  )`--.  .--'|  | |(OO )|  |  ' (|  '--.                 
+                        (|  .-.  ||  .___.'   |  |  (|  |_/    |  |   |  | | |  \|  |  / :|  .--'                 
+                         |  | |  ||  |        |  |   |  |'->   |  |   \  '-'(_ .'|  '-'  /|  `---.                
+                         `--' `--'`--'        `--'   `--'      `--'    `-----'   `------' `------'";
+            Console.WriteLine(ascii);
+
+            Console.WriteLine("\n \n hey sprout...!");
+
+            //menu loop
             int i = 1;
             while (i > 0)
             {
-                Console.WriteLine("Would you like to know what vegetable you'll grow into?");
-                Console.WriteLine("1: Continue");
-                Console.WriteLine("2: No I do not.");
-                Console.WriteLine("3. Search for previous results");
+                Console.WriteLine(" Would you like to know what vegetable you'll grow into?");
+                Console.WriteLine(" 1: Continue");
+                Console.WriteLine(" 2: No I do not.");
+                Console.WriteLine(" 3. Search for previous results");
 
-                int entry = Convert.ToInt32(Console.ReadLine()); // turns string response into integer to read
+                int entry = Convert.ToInt32(Console.ReadLine()); //turns string response into integer to read
                 
-                if (entry == 1) // initializes the quiz
+                if (entry == 1) //initializes the quiz
                 {
-                    Console.WriteLine("Okay...let's start...");
+                    Console.WriteLine("\n Okay...let's start...");
 
-                    Console.WriteLine("Adding new user! Name: ");
+                    Console.WriteLine("\n Adding new user! Name: ");
                     string Taker = Console.ReadLine();
                     _takers.Add(new Quiz() { Taker = Taker });
                     var newTaker = _takers.FirstOrDefault(x => x.Taker == Taker);
                     newTaker.Questions();
                     newTaker.Result();
                 }
-                else if (entry == 2) // exit
+                else if (entry == 2) //exit
                 {
                     i = 0;
                 }
-                else if (entry == 3)
+                else if (entry == 3) //prints list of previous results and uses linq query to find taker
                 {
-                    Console.WriteLine("Search for other results.");
+                    Console.WriteLine("\n Search for other results.");
 
                     if (_takers.Count == 0)
                     {
-                        Console.WriteLine("No one has taken it yet");
+                        Console.WriteLine("\n No one has taken it yet");
                     }
                     else
                     {
-                        Console.WriteLine("Here are the takers:");
+                        Console.WriteLine("\n Here are the previous takers:\n");
                         for (int x = 0; x < _takers.Count; x++)
                         {
-
-                            Console.WriteLine($"{_takers[x].Taker}");
-
-
+                            Console.WriteLine($"\n {_takers[x].Taker}\n");
                         }
-                        Console.WriteLine("Enter the taker name: ");
+                        Console.WriteLine("\n Enter the taker name: \n");
                         string searchTaker = Console.ReadLine();
 
-                        // use a linq query to find someone
+                        //use a linq query to find taker
                         var taker = _takers.FirstOrDefault(x => x.Taker == searchTaker);
 
-                        // do they exist?
+                        //do they exist?
                         if (taker != null)
                         {
-                            // user isnt null and runs their greeting function
+                            //taker isnt null and runs their result
                             taker.Result();
                         }
                         else
                         {
-                            // user is null
-                            Console.WriteLine($"{searchTaker} not found");
-
+                            //taker is null
+                            Console.WriteLine($"\n {searchTaker} not found");
                         }
-
-                  
-
                     
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Not a valid entry."); // re-start loop
+                    Console.WriteLine("\n Not a valid entry."); //re-start search taker loop
 
                 }
             }
--- VegetablesAptitude/VegetablesAptitude/quiz.cs
+++ VegetablesAptitude/VegetablesAptitude/quiz.cs
@@ -5,19 +5,21 @@ namespace VegetablesAptitude
 {
 	public class Quiz
 	{
-        public string Taker { get; set; } //name variable
+        public string Taker { get; set; } //name variable. new instance of Quiz is called Taker
 
         //score integers
         public int ScoreInterface = 0;
         public int ScoreEthics = 0;
         public int ScoreEssence = 0;
 
+        //score strings
         public string Interface;
         public string Ethics;
         public string Essence;
 
+        //vegetable generation
         public string veggie = Vegetable();
-
+        //get random vegetable from text file
         public static string Vegetable()
         {
             string[] Veggie = System.IO.File.ReadAllLines(@"..\text files\Vegetables.txt");
@@ -28,11 +30,10 @@ namespace VegetablesAptitude
 
         } 
         
-
         public void Questions()
         {
            
-            // creates array with questions read from file
+            //creates array with questions read from file
             string[] Qs = System.IO.File.ReadAllLines(@"..\text files\Questions.txt");
 
             while (Qs.Length > 0)
@@ -48,23 +49,22 @@ namespace VegetablesAptitude
                 //converts first character of array to correspond to category of question into an integer for use in switch case
                 int questionType = Convert.ToInt32(Qs[number].Substring(0, 1));
 
-                string questionPrompt = Qs[number].Remove(0, 2); // removes 2 characters starting at 0 (1,)
+                string questionPrompt = Qs[number].Remove(0, 2); //removes 2 characters starting at 0 (1,)
 
                 //clear console and write new question
                 Console.Clear();
                 
-
-                
+                //exception handling bool
                 bool isReplyValid;
 
-                do
+                do //exception handling do/while loop that repeats question until isReplyValid = true;
                 {
-                    Console.WriteLine($"{Taker}, {questionPrompt}");
-                    Console.WriteLine("Type (1) for yes \nType (2) for no");
+                    Console.WriteLine($"\n {Taker}, {questionPrompt}");
+                    Console.WriteLine(" Type 1 for yes \n Type 2 for no");
                     string reply = Console.ReadLine();
                     if (reply == "1")
                     {
-                        switch (questionType)
+                        switch (questionType) //applies scores to different categories of questions
                         {
                             case 1:
                                 ScoreInterface++;
@@ -81,7 +81,7 @@ namespace VegetablesAptitude
                     else if (reply == "2") {
                         isReplyValid = true;
                     }
-                    else
+                    else //throws error
                     {
                         isReplyValid = false;
                         Console.WriteLine("invalid input, press enter");
@@ -90,19 +90,13 @@ namespace VegetablesAptitude
                     
                     Console.Clear();
 
-                } while (!isReplyValid);
-             
-                    
-     
-     
+                } while (!isReplyValid); //false response throws back to beginning of do/while 
 
-                Qs = questionsList.ToArray();
-
-               
+                Qs = questionsList.ToArray(); //convert list to array which feeds back into the use of Qs array at the beginning of while loop
             }
 
         }
-        public string ScoreStringify()
+        public string ScoreStringify() //converts scores from different categories into strings for the result output 
         {
             if (ScoreInterface <= 1) Interface = "thoughtful";
             if (ScoreInterface == 2) Interface = "intimate";
@@ -116,19 +110,15 @@ namespace VegetablesAptitude
             if (ScoreEssence == 2) Essence = "out of bounce";
             if (ScoreEssence == 3) Essence = "existential";
 
-            return $"{Interface}, {Ethics}, and {Essence} {veggie}";
+            return $"{Interface},\n {Ethics},\n and {Essence}\n {veggie}! \n \n Press any key...";
         }
 
         public void Result() // result output with name, 3 scores, and vegetable
         {
-            Console.WriteLine($"{Taker} is a {ScoreStringify()}!");
+            Console.WriteLine($"\n {Taker} is a\n {ScoreStringify()}");
             Console.ReadLine();
             Console.Clear();
         }
-
-
-
-
 
     }
 }
